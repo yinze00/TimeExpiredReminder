c29f2cc0a078cd548a524d2177790ac0b6f3afb7
junwei.wang <junwei.wang@zju.edu.cn>
2024-07-11 16:49:40 +0800

    update

--- background.js
+++ background.js
@@ -19,6 +19,48 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     }
 });
 
+function getTabTitle(tabId) {
+    return new Promise((resolve, reject) => {
+        chrome.tabs.get(tabId, tab => {
+            if (chrome.runtime.lastError) {
+                reject(new Error(chrome.runtime.lastError.message));
+            } else {
+                resolve(tab.title);
+            }
+        });
+    });
+}
+
+function createNotification(tabId, title) {
+    chrome.notifications.create(
+        'notificationId', // Notification ID
+        {
+            type: 'basic',
+            iconUrl: 'urgent.png',
+            title: '快看一眼',
+            message: `${title}`
+        },
+        (notificationId) => {
+            if (chrome.runtime.lastError) {
+                console.error(chrome.runtime.lastError);
+            } else {
+                console.log('Notification shown with ID:', notificationId);
+            }
+        }
+    );
+
+    // 监听通知点击事件
+    chrome.notifications.onClicked.addListener(function (notificationId) {
+        if (notificationId === 'notificationId') {
+            // 激活指定的标签页
+            chrome.tabs.update(tabId, { active: true }, function (tab) {
+                chrome.windows.update(tab.windowId, { focused: true });
+            });
+        }
+    });
+}
+
+
 function startTimer(tabId, minutes) {
     if (timers[tabId]) {
         clearInterval(timers[tabId].interval);
@@ -38,21 +80,43 @@ function updateIcon(tabId) {
             delete timers[tabId];
 
             // Switch to the tab
-            chrome.tabs.update(tabId, { active: true }, () => {
-                // Show notification
-                chrome.notifications.create('', {
-                    type: 'basic',
-                    iconUrl: 'clock48.png',
-                    title: 'Tab Timer Finished',
-                    message: `The timer for tab ${tabId} has finished!`
-                }, (notificationId) => {
-                    if (chrome.runtime.lastError) {
-                        console.error(chrome.runtime.lastError);
-                    } else {
-                        console.log('Notification shown with ID:', notificationId);
-                    }
+            getTabTitle(tabId)
+                .then(title => {
+                    createNotification(tabId, title)
+                    // chrome.tabs.update(tabId, { active: true }, () => {
+                    //     // Show notification
+                    //     chrome.notifications.create('', {
+                    //         type: 'basic',
+                    //         iconUrl: 'urgent.png',
+                    //         title: '快看一眼 ',
+                    //         message: `${title}`
+                    //     }, (notificationId) => {
+                    //         if (chrome.runtime.lastError) {
+                    //             console.error(chrome.runtime.lastError);
+                    //         } else {
+                    //             console.log('Notification shown with ID:', notificationId);
+                    //         }
+                    //     });
+                    // });
+                })
+                .catch(error => {
+                    console.error('Error fetching tab title:', error.message);
                 });
-            });
+            // chrome.tabs.update(tabId, { active: true }, () => {
+            //     // Show notification
+            //     chrome.notifications.create('', {
+            //         type: 'basic',
+            //         iconUrl: 'clock48.png',
+            //         title: 'Tab Timer Finished',
+            //         message: `The timer for tab ${tabId} has finished!`
+            //     }, (notificationId) => {
+            //         if (chrome.runtime.lastError) {
+            //             console.error(chrome.runtime.lastError);
+            //         } else {
+            //             console.log('Notification shown with ID:', notificationId);
+            //         }
+            //     });
+            // });
         } else {
             let minutes = Math.floor(remaining / 60000);
             let seconds = Math.floor((remaining % 60000) / 1000);
